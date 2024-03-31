e64a60b63bf90dc61bad72bbf1dddb6cfd8c002b
campos <nonesovile96@hotmail.com>
2024-03-30 22:45:45 -0300

    Enable tmux

--- home-manager/campos/programs.nix
+++ home-manager/campos/programs.nix
@@ -16,4 +16,24 @@
       theme = "flazz";
     };
   };
+
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    clock24 = true;
+    disableConfirmationPrompt = true;
+
+    newSession = true;
+    escapeTime = 0;
+    secureSocket = false;
+
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
+
+    extraConfig = ''
+      # Mouse works as expected
+      set-option -g mouse on
+    '';
+  };
 }
