73908c8c261b3ebe6a6f02fe15222fde294b64b5
campos <nonesovile96@hotmail.com>
2024-04-02 22:18:00 -0300

    Enable nix-ld

--- hosts/PC-Sala/configuration.nix
+++ hosts/PC-Sala/configuration.nix
@@ -155,6 +155,9 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
 
+  # Enable nix-ld
+  programs.nix-ld.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
